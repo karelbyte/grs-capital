c4917f7046d0e36a547aad472484d712a1bb1f37
karel <karelpuerto78@gmail.com>
2020-06-23 17:21:46 -0500

    ajustes en los idiomas del parallax

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -7,6 +7,18 @@ return [
     'nav-projects' => 'PROYECTOS',
     'nav-about' => 'NOSOTROS',
     'nav-contact' => 'CONTACTO',
-
+ // TEXTO DE LOS SLIDES PARALLAX
+    'parallax-head-1' => 'Somos el modo inteligente!',
+    'parallax-content-1' => 'La práctica y el conocimiento de utilizar el
+                                dinero de modo inteligente, es una de las habilidades menos desarrolladas que poseemos.
+                                Aprender a invertir y transmitir ese conocimiento
+                                a nuestros hijos no es un mero divertimento,
+                                es una obligación ineludible.',
+    'parallax-head-2' => 'Respuesta Mágica!',
+    'parallax-content-2' => 'Le garantizamos que funciona, a nosotros en GRS nos ha funcionado magníficamente. Aquí se la podemos sintetizar de un modo sencillo,'.
+                                'usted no tiene que buscar <b>"la riqueza"</b> como un concepto abstracto, su búsqueda tiene que orientarse a lograr la <b>independencia financiera.</b>',
+    'parallax-head-3' => ' El dinero nunca duerme!',
+    'parallax-content-3' => 'La independencia financiera se logra aprendiendo a invertir para conseguir ingresos pasivos y continuar con esa actividad hasta que los mismos superen sus gastos habituales.',
+    'parallax-contact' => 'Contactanos',
 
 ];
