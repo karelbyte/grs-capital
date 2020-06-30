62ae7cd1643de11acfe4409f93c3b07189340e42
karel <karelpuerto78@gmail.com>
2020-06-30 14:17:44 -0500

    ajusestes en el footer

--- resources/lang/vendor/cookieConsent/en/texts.php
+++ resources/lang/vendor/cookieConsent/en/texts.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'message' => 'We use cookies to ensure you get the best experience on our website.',
+    'message' => 'This website uses cookies, both its own and those of third parties, to improve the user experience. If you continue browsing, we consider that you accept their use.',
     'agree' => 'Allow cookies',
     'more' => 'More information'
 ];
