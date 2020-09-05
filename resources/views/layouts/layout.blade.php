80bd9cbe4dcc85d5af9b6efa2a4caceef0e2bed5
karel <karelpuerto78@gmail.com>
2020-09-05 09:37:47 -0500

    ajustando la traduccion

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -28,7 +28,6 @@
     </div>
 </div>
 @yield('content')
-<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FElpuertodigital-101480131669910&tabs=messages&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe><!-- Footer START -->
 @component('components.footer')@endcomponent
 @include('cookieConsent::index')
 <!-- Scroll to top button Start -->
